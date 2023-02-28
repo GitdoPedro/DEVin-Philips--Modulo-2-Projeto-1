fd1ab54f0ed52922bd7e864f1c64002fa9265080
GitdoPedro <emailpedrolima@gmail.com>
2023-02-28 15:49:29 -0300

    Menu inicial

--- src/Telas.java
+++ src/Telas.java
@@ -1,3 +1,74 @@
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.Scanner;
+
 public class Telas {
+
+    private Scanner entradaUsuario = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Telas tela = new Telas();
+        tela.inicio();
+    }
+
+    void inicio()  {
+        int opcao;
+
+        try {
+              opcao = menuInicial();
+              switch (opcao){
+                    case 1:
+                        cadastroPaciente();
+                        break;
+                    case 2:
+                        cadastroEnfermeiro();
+                        break;
+                    case 3:
+                        cadastroMedico();
+                        break;
+                    case 4:
+                        atendimentoMedico();
+                        break;
+                    case 5:
+                        atualizacaoStatusPaciente();
+                        break;
+                    case 6:
+                        relatorios();
+                        break;
+                  default:
+                      System.out.println("Opção inexistente");
+                        break;
+            }
+        } catch (Exception e) {
+            System.out.println("Opção invalida");
+            inicio();
+        }
+    }
+
+    private int menuInicial() throws Exception{
+        String[] menuInicial = {
+                "Bem vindo ao sistema LABMedicine",
+                "=======================================================",
+                "Selecione uma das opções abaixo",
+                "1. Cadastro de Paciente",
+                "2. Cadastro de Enfermeiro",
+                "3. Cadastro de Médico",
+                "4. Realização de Atendimento Médico",
+                "5. Atualização do Status de Atendimento do Paciente",
+                "6. Relatórios",
+                "Número selecionado: "
+        };
+        for (String linha: menuInicial) {
+            System.out.println(linha.toString());
+
+        }
+        int opcaoEntrada = entradaUsuario.nextInt();
+        if (opcaoEntrada < 1 || opcaoEntrada > 6){
+            throw new Exception();
+        }else{
+            return opcaoEntrada;
+        }
+    }
+
 
 }
